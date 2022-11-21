f8ef44814dd7dbe5974bdf06208d4a6742c04158
anuragkalra <anuragkalra@outlook.com>
2022-11-21 02:31:12 -0500

    fixed API key

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = 'ff7f67f16bc6ba36be57471dac17f9cf';
+const API_KEY = 'd5eb33df77f10ecb78db00a12a50e458';
 
 class App extends React.Component {
   state = {
@@ -18,9 +18,11 @@ class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
+    const country = e.target.elements.country.value
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
+    console.log(API_KEY)
+    console.log(data)
     if (city && country) {
       this.setState({
         temperature: data.main.temp,
